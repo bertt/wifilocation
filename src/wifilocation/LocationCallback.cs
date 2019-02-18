188cc1ba377184443883af2cf71ac7728e38ffac
bert <btemme@hotmail.com>
2019-02-18 18:15:33 +0100

    initial

---
+++ src/wifilocation/LocationCallback.cs
@@ -0,0 +1,35 @@
+﻿using System.Linq;
+using Android.Gms.Location;
+using Android.Util;
+
+namespace wifilocation
+{
+    public class FusedLocationProviderCallback : LocationCallback
+    {
+        readonly MainActivity activity;
+
+        public FusedLocationProviderCallback(MainActivity activity)
+        {
+            this.activity = activity;
+        }
+
+        public override void OnLocationAvailability(LocationAvailability locationAvailability)
+        {
+            Log.Debug("FusedLocationProviderSample", "IsLocationAvailable: {0}", locationAvailability.IsLocationAvailable);
+        }
+
+        public override void OnLocationResult(LocationResult result)
+        {
+            if (result.Locations.Any())
+            {
+                var location = result.Locations.First();
+                var loc = $"{location.Longitude}, {location.Latitude}, {location.Accuracy}";
+                activity.DisplayText(loc);
+            }
+            else
+            {
+                // No locations to work with.
+            }
+        }
+    }
+}
---
+++ src/wifilocation/MainActivity.cs
@@ -0,0 +1,98 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Views;
+using Android.Gms.Common;
+using Android.Gms.Location;
+using Android.Util;
+using System.Threading.Tasks;
+using Android.Widget;
+
+namespace wifilocation
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme.NoActionBar", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        private FusedLocationProviderClient fusedLocationProviderClient;
+        private LocationRequest locationRequest;
+        private LocationCallback locationCallback;
+        private TextView textViewHello;
+
+        protected override async void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            SetContentView(Resource.Layout.activity_main);
+
+            Android.Support.V7.Widget.Toolbar toolbar = FindViewById<Android.Support.V7.Widget.Toolbar>(Resource.Id.toolbar);
+            SetSupportActionBar(toolbar);
+            textViewHello = FindViewById<TextView>(Resource.Id.txtHello);
+
+            IsGooglePlayServicesInstalled();
+            fusedLocationProviderClient = LocationServices.GetFusedLocationProviderClient(this);
+
+
+            locationRequest = new LocationRequest()
+                                  .SetPriority(LocationRequest.PriorityHighAccuracy)
+                                  .SetInterval(1000)
+                                  .SetFastestInterval(1000);
+            locationCallback = new FusedLocationProviderCallback(this);
+
+            await StartRequestingLocationUpdates();
+        }
+
+        async Task StartRequestingLocationUpdates()
+        {
+            await fusedLocationProviderClient.RequestLocationUpdatesAsync(locationRequest, locationCallback);
+        }
+
+        public override bool OnCreateOptionsMenu(IMenu menu)
+        {
+            MenuInflater.Inflate(Resource.Menu.menu_main, menu);
+            return true;
+        }
+
+        public override bool OnOptionsItemSelected(IMenuItem item)
+        {
+            int id = item.ItemId;
+            if (id == Resource.Id.action_settings)
+            {
+                return true;
+            }
+
+            return base.OnOptionsItemSelected(item);
+        }
+
+
+        bool IsGooglePlayServicesInstalled()
+        {
+            var queryResult = GoogleApiAvailability.Instance.IsGooglePlayServicesAvailable(this);
+            if (queryResult == ConnectionResult.Success)
+            {
+                Log.Info("MainActivity", "Google Play Services is installed on this device.");
+                return true;
+            }
+
+            if (GoogleApiAvailability.Instance.IsUserResolvableError(queryResult))
+            {
+                // Check if there is a way the user can resolve the issue
+                var errorString = GoogleApiAvailability.Instance.GetErrorString(queryResult);
+                Log.Error("MainActivity", "There is a problem with Google Play Services on this device: {0} - {1}",
+                          queryResult, errorString);
+
+                // Alternately, display the error to the user.
+            }
+
+            return false;
+        }
+
+
+        public void DisplayText(string text)
+        {
+            if (text != "")
+            {
+                textViewHello.Text = text;
+            }
+        }
+    }
+}
+
